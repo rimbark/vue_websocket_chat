712795e28ef06a9b420a7fd97eade1ce55468e5d
Dzane <dzane@student.21-school.ru>
2023-10-08 20:16:16 +0700

    рефакторинг выбора канала

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+// eslint-disable-next-line sort-imports
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+export default defineConfig ({
+  plugins: [vue ()],
+  base: "./"
 })
